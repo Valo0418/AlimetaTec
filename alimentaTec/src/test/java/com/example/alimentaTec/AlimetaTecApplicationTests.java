b39233d8279a9dbb5e556718f67d88bd952a23e0
Valo0418 <160807871+Valo0418@users.noreply.github.com>
2024-09-24 10:50:43 -0600

    Creacion de la api
    añado las carpetas y la creacion de la api

---
+++ alimentaTec/src/test/java/com/example/alimentaTec/AlimetaTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alimentaTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimetaTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
