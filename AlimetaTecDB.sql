f1370bc27d81b5f389c1d088196506024893c0ed
celio bonilla <108300688+celiobovatola@users.noreply.github.com>
2024-10-09 12:57:53 -0600

    MODIFICATION OF THE DATABASE

--- AlimetaTecDB.sql
+++ AlimetaTecDB.sql
@@ -1,23 +1,19 @@
 user = "alimentador"
 password = "6X6[Q&W6pKsw"
 
-create database mapachebigoton;
 create database AlimetaTecDB;
 drop AlimetaTecDB;
 use AlimetaTecDB;
 
-create table actividadfisica(
-	id_actividad INT AUTO_INCREMENT,
-    nombre_actividad VARCHAR(100) NOT NULL,
-    duracion time not null,
-    calorias_quemadas  int ,
-    intensidad ENUM('baja', 'media', 'alta'),
-    primary key idActividad_AF_PK (id_actividad)
+/*CREACION DE LA TABLA DE ACTIVIDADES FISICAS*/
+create table PhysicalActivity(
+	idActivity INT AUTO_INCREMENT,
+    nameActivity VARCHAR(100) NOT NULL,
+    duration time not null,
+    caloriesBurned  int ,
+    intensity ENUM('low', 'medium', 'high'),
+    primary key idActivity_AF_PK (idActivity)
 );
-
-INSERT INTO actividad_fisica (nombre_actividad, duracion, calorias_quemadas, intensidad, fecha) VALUES ('Correr', '00:30:00', 250, 'media', '2023-11-20');
-USE alimetatecdb; -- Selecciona la base de datos
-INSERT INTO actividad_fisica (nombre_actividad, duracion, calorias_quemadas, intensidad, fecha) VALUES ('Correr', '00:30:00', 250, 'media', '2023-11-20');
-
-INSERT INTO actividadfisica (nombre_actividad, duracion, calorias_quemadas, intensidad)
-VALUES ('Correr', '00:30:00', 300, 'media');
+TRUNCATE `alimetatecdb`.`physicalactivity`;
+DROP TABLE `alimetatecdb`.`physicalactivity`;
+INSERT INTO PhysicalActivity (nameActivity, duration, caloriesBurned, intensity) VALUES ('Run', '00:30:00', 300, 'medium');
