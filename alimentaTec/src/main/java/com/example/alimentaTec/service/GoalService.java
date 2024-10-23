fd5a5e54541517699124ad0586379a783e12c692
Valo0418 <160807871+Valo0418@users.noreply.github.com>
2024-10-22 23:01:03 -0600

    Borrado
    Borre unas importaciones que ya no ocupaba en el formulario

--- alimentaTec/src/main/java/com/example/alimentaTec/service/GoalService.java
+++ alimentaTec/src/main/java/com/example/alimentaTec/service/GoalService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.alimentaTec.model.Goal;
-import com.example.alimentaTec.model.Saucer;
+
 import com.example.alimentaTec.repository.GoalRepository;
 import jakarta.transaction.Transactional;
 
