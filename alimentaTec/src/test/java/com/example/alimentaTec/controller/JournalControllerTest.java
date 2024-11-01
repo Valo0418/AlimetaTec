f90eab507ef18a8528984b09720b24ed14d66894
celio bonilla <108300688+celiobovatola@users.noreply.github.com>
2024-11-01 12:58:58 -0600

    Merge branch 'main' into Mariana

---
+++ alimentaTec/src/test/java/com/example/alimentaTec/controller/JournalControllerTest.java
@@ -0,0 +1,62 @@
+package com.example.alimentaTec.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JournalControllerTest {
+    
+     @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JournalController controller;
+
+
+    @Test 
+    void contextLoads () throws Exception{
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void getAllPaginatedTest() throws Exception {
+        mvc.perform(get("/journals/pagination")
+                .param("page", "0")
+                .param("size", "10")
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(greaterThan(0))));
+    }
+
+    @Test
+    public void getByIdTest() throws Exception {
+        mvc.perform(get("/journals/2").accept(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.idJournal", is(2)));
+    }
+
+    @Test
+    public void getByIdNotFoundTest() throws Exception {
+        mvc.perform(get("/journals/0").accept(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(containsString("The requested item is not registered")));
+    }
+}
